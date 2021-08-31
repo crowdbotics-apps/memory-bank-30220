a9a5eaf6978f487465724fc97606fddadff895f9
Crowdbotics <team@crowdbotics.com>
2021-08-31 12:12:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "memory_bank_30220Identifier",
+  url: "https://memory_bank_30220Identifier.botics.co",
+  api: "https://memory_bank_30220Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
